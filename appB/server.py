c1e06d7892fe065a03526ae6c293cd9feef6eb79
jh2946 <junhong2946@gmail.com>
2024-07-21 14:57:05 +0800

    hi

--- appB/server.py
+++ appB/server.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 @app.route('/')
 def home(): return '<h1>appB</h1>'
 @app.route('/about')
-def about(): return 'about'
+def about(): return 'asdf'
 app.run(host='0.0.0.0', port=80)
