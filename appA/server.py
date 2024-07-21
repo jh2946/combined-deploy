4e0c6af1750729f655d9ce5667191a92e7a40e43
jh2946 <junhong2946@gmail.com>
2024-07-21 14:39:21 +0800

    wtf why beforeinstall not running

--- appA/server.py
+++ appA/server.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 @app.route('/')
 def home(): return '<h1>appA</h1>'
 @app.route('/about')
-def about(): return 'testing'
+def about(): return 'about'
 app.run(host='0.0.0.0', port=80)
