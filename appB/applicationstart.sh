ccb48c760696a35582a260c41af5ac3d4c2fbb61
jh2946 <junhong2946@gmail.com>
2024-07-21 14:16:29 +0800

    Initial commit

---
+++ appB/applicationstart.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo nohup python3 server.py > script.log 2>&1 &
---
+++ appB/server.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home(): return '<h1>appB</h1>'
+@app.route('/about')
+def about(): return 'about'
+app.run(host='0.0.0.0', port=80)
